a43ceccf1754c32a3a0f001821dc725d9325258d
Swannhs <princeapurbask@gmail.com>
2021-01-22 12:40:25 -0500

    Get user's location

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactDom from 'react-dom';
 
 function App() {
+window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    error => console.log(error)
+);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
